90506451fa98c3f4c380ef60a88da7877b09c9e7
pradhan-not-found <souradeeppradhan7@gmail.com>
2026-08-04 23:31:22 +0530

    fix: change content-type to text/plain for no-cors fetch

--- landing-page/src/App.tsx
+++ landing-page/src/App.tsx
@@ -17,7 +17,7 @@ function App() {
           method: 'POST',
           mode: 'no-cors',
           headers: {
-            'Content-Type': 'application/json',
+            'Content-Type': 'text/plain;charset=utf-8',
           },
           body: JSON.stringify({ email: email.trim() }),
         });
