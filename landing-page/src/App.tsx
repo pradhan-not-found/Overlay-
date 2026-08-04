9a1ba6e21a2a31d0b34fd4500a86795c8b842027
pradhan-not-found <souradeeppradhan7@gmail.com>
2026-08-05 00:00:40 +0530

    style: remove 'Available on' text

--- landing-page/src/App.tsx
+++ landing-page/src/App.tsx
@@ -86,7 +86,6 @@ function App() {
 
         {/* Supported Platforms */}
         <div className="supported-platforms">
-          <span className="platform-text">Available on</span>
           <div className="platform-icons">
             {/* Windows */}
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor"><path d="M0 3.449L9.75 2.1v9.451H0m10.949-9.602L24 0v11.4H10.949M0 12.6h9.75v9.451L0 20.699M10.949 12.6H24V24l-12.951-1.801"/></svg>
