e26c0da816b70a8dd5c0eb4fb18c52be5911963a
pradhan-not-found <souradeeppradhan7@gmail.com>
2026-08-05 21:06:21 +0530

    style: use client-provided logo images for download buttons with white scaling

--- landing-page/src/App.tsx
+++ landing-page/src/App.tsx
@@ -58,15 +58,15 @@ function App() {
           </p>
           <div className="download-buttons" style={{ display: 'flex', flexWrap: 'wrap', gap: '16px', width: '100%' }}>
             <a href="#" className="dl-btn">
-              <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor"><path d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm1.832 14.76v3.48H11.02V7.44h4.944c2.196 0 3.732 1.488 3.732 3.66 0 2.184-1.536 3.66-3.732 3.66h-2.136zm0-4.992v2.688h1.992c.708 0 1.236-.456 1.236-1.344 0-.888-.528-1.344-1.236-1.344h-1.992z"/></svg>
+              <img src="/logo/producthunt.png" alt="Product Hunt" style={{ width: '20px', height: '20px', objectFit: 'contain', filter: 'brightness(0) invert(1)' }} />
               Product Hunt
             </a>
             <a href="#" className="dl-btn">
-              <svg width="20" height="20" viewBox="0 0 384 512" fill="currentColor"><path d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"/></svg>
+              <img src="/logo/apple.png" alt="Mac" style={{ width: '20px', height: '20px', objectFit: 'contain', filter: 'brightness(0) invert(1)' }} />
               Download for Mac
             </a>
             <a href="#" className="dl-btn">
-              <svg width="20" height="20" viewBox="0 0 448 512" fill="currentColor"><path d="M0 93.7l183.6-25.3v177.4H0V93.7zm0 324.6l183.6 25.3V268.4H0v149.9zm203.8 28L448 31.6v214.2H203.8v200.6zm0-208V27.4L448 0v238.4H203.8z"/></svg>
+              <img src="/logo/windows.png" alt="Windows" style={{ width: '20px', height: '20px', objectFit: 'contain', filter: 'brightness(0) invert(1)' }} />
               Download for Windows
             </a>
           </div>
